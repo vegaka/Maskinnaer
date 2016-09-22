cb39a6ea198fca968bec610552fdbb25edff80ee
Daniel Fedai Larsen <daniefla@stud.ntnu.no>
2016-09-22 13:09:39 +0200

    first C test

--- share/ex2/gpio.c
+++ share/ex2/gpio.c
@@ -7,6 +7,9 @@
 void setupGPIO()
 {
 	/* TODO set input and output pins for the joystick */
+	*GPIO_PC_MODEL = 0x33333333;
+	*GPIO_PC_DOUT = 0xff;
+
 
 	/* Example of HW access from C code: turn on joystick LEDs D4-D8
 	   check efm32gg.h for other useful register definitions
@@ -15,4 +18,10 @@ void setupGPIO()
 	*GPIO_PA_CTRL = 2;	/* set high drive strength */
 	*GPIO_PA_MODEH = 0x55555555;	/* set pins A8-15 as output */
 	*GPIO_PA_DOUT = 0x0700;	/* turn on LEDs D4-D8 (LEDs are active low) */
+
+	while (true)
+	{
+		*GPIO_PA_DOUT = *GPIO_PC_DIN << 8;
+		//*GPIO_PA_DOUT = *GPIO_PC_DIN;
+	}
 }
