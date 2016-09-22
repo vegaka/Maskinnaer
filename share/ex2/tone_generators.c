2855af442e4553b8916fddb3edb94b9213611c25
Johannes Andersen <johanes1@online.no>
2016-09-22 12:40:52 +0200

    Added tone generators

---
+++ share/ex2/tone_generators.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "tone_generators.h"
+
+#define PI 3.14159265
+
+int sineWave(int freq, int time, int amp, int samplingFreq) {
+    printf("%f", amp * sin(2 * PI * time/samplingFreq * freq) / 2);
+    return amp/2 * sin(2 * PI * time/samplingFreq * freq);
+}
+
+int squareWave(int freq, int time, int amp, int samplingFreq) {
+    printf("%f", amp * (sin(2 * PI * time/samplingFreq * freq) > 0 ? 1 : -1 ) / 2);
+    return amp/2 * (sin(2 * PI * time/samplingFreq * freq) > 0 ? 1 : -1 );
+}
+
+int triangleWave(int freq, int time, int amp, int samplingFreq) {
+    double progress = (double)time*freq/(samplingFreq) - floor(time*freq/(samplingFreq));
+    printf("%f", amp * ((progress < 0.5 ? progress : 1- progress)*2-0.5));
+    return amp * ((progress < 0.5 ? progress : 1- progress)*2-0.5);
+}
+
+int sawtoothWave(int freq, int time, int amp, int samplingFreq) {
+    double progress = (double)time*freq/(samplingFreq) - floor(time*freq/(samplingFreq));
+    printf("%f", amp * ((progress) -0.5));
+    return amp * (progress - 0.5);
+}
---
+++ share/ex2/tone_generators.h
@@ -0,0 +1,7 @@
+#ifndef MASKINNAER_TONE_GENERATORS_H
+#define MASKINNAER_TONE_GENERATORS_H
+    int sineWave(int frequency, int time, int amplitude, int samplingFrequency);
+    int squareWave(int frequency, int time, int amplitude, int samplingFrequency);
+    int triangleWave(int frequency, int time, int amplitude, int samplingFrequency);
+    int sawtoothWave(int frequency, int time, int amplitude, int samplingFrequency);
+#endif //MASKINNAER_TONE_GENERATORS_H
