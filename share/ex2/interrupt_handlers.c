01e9f5c16d38b323af7eea094008e4a288a69a1a
Vegard Seim Karstang <vegard.karstang@gmail.com>
2016-10-07 11:39:57 +0200

    Added playing sounds when pressing buttons.

--- share/ex2/interrupt_handlers.c
+++ share/ex2/interrupt_handlers.c
@@ -22,14 +22,29 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 
 	if (noteArrayIndex < 23)
 	{
-		*DAC0_CH0DATA = sineWave(startUpSoundLeft[index], counter, 4096 / 2, 25000, &phaseLeft);
-		*DAC0_CH1DATA = sineWave(startUpSoundRight[index] * 2, counter, 4096 / 2, 25000, &phaseRight);
+		uint32_t sample = sineWave(startUpSoundLeft[index], counter, 4096 / 2, 25000, &phaseLeft);
+		*DAC0_CH0DATA = sample;
+		*DAC0_CH1DATA = sample;
 	}*/
 
-	if (counter < 1024 * 10)
+	if (hit_counter < 1024)
 	{
-		*DAC0_CH0DATA = win_effect(counter);
-		*DAC0_CH1DATA = win_effect(counter);
+		effect_sample = hitEffect(hit_counter);
+		*DAC0_CH0DATA = effect_sample;
+		*DAC0_CH1DATA = effect_sample;
+		hit_counter++;
+	} else if (metal_counter < 1024)
+	{
+		effect_sample = metal_effect(metal_counter);
+		*DAC0_CH0DATA = effect_sample;
+		*DAC0_CH1DATA = effect_sample;
+		metal_counter++;
+	} else if (win_counter < 1024 * 10)
+	{
+		effect_sample = win_effect(win_counter);
+		*DAC0_CH0DATA = effect_sample;
+		*DAC0_CH1DATA = effect_sample;
+		win_counter++;
 	}
 
 
@@ -45,21 +60,40 @@ void __attribute__ ((interrupt)) TIMER1_IRQHandler()
 /* GPIO even pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler()
 {
-	/* TODO handle button pressed event, remember to clear pending interrupt */
-	*GPIO_PA_DOUT = ~(*GPIO_PA_DOUT);
-	counter = 0;
-
-	// Cleanup
-	*GPIO_IFC = *GPIO_IF;
+	handle_GPIO_interrupt();
 }
 
 /* GPIO odd pin interrupt handler */
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler()
 {
+	handle_GPIO_interrupt();
+}
+
+void handle_GPIO_interrupt()
+{
 	/* TODO handle button pressed event, remember to clear pending interrupt */
-	*GPIO_PA_DOUT = ~(*GPIO_PA_DOUT);
-	counter = 0;
+	uint16_t interrupt_flags = *GPIO_IF;
+	uint16_t current_buttons = ~ *GPIO_PC_DIN;
+
+	int play_hit_effect = (interrupt_flags & 1) & current_buttons;
+	int play_metal_effect = (interrupt_flags & (1 << 1)) & current_buttons;
+	int play_win_effect = (interrupt_flags & (1 << 2)) & current_buttons;
+
+	if (play_hit_effect)
+	{
+		hit_counter = 0;
+	}
+
+	if (play_metal_effect)
+	{
+		metal_counter = 0;
+	}
+
+	if (play_win_effect)
+	{
+		win_counter = 0;
+	}
 
 	// Cleanup
-	*GPIO_IFC = *GPIO_IF;
+	*GPIO_IFC = *GPIO_IF;	
 }
--- share/ex2/interrupt_handlers.h
+++ share/ex2/interrupt_handlers.h
@@ -5,8 +5,16 @@ uint32_t counter = 0;
 uint32_t phaseLeft = 0;
 uint32_t phaseRight = 0;
 
+uint32_t hit_counter = 0;
+uint32_t metal_counter = 0;
+uint32_t win_counter = 0;
+uint32_t effect_sample = 0;
+
+uint32_t buttons_pressed = 0;
+
 void __attribute__ ((interrupt)) TIMER1_IRQHandler();
 void __attribute__ ((interrupt)) GPIO_EVEN_IRQHandler();
 void __attribute__ ((interrupt)) GPIO_ODD_IRQHandler();
+void handle_GPIO_interrupt();
 
 #endif
