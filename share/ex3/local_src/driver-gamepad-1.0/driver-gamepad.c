37f56af317b07d22790464a8e17b0114c1a35450
Vegard Seim Karstang <vegard.karstang@gmail.com>
2016-10-28 14:28:06 +0200

    Changed to using the old way of writing drivers. Interrupts work now.

--- share/ex3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ share/ex3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -2,71 +2,24 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/ioport.h>
-#include <linux/platform_device.h>
 #include <linux/interrupt.h>
 
-#define DEVICE_NAME "TDT4258 gamepad driver"
+#include <asm/io.h>
 
-int register_irq(int irq_num);
+#include "efm32gg.h"
 
-static irqreturn_t gpio_handler(int irq, void *dev_id, struct pt_regs *regs)
+#define DEVICE_NAME "TDT4258"
+
+#define IO_REGION_SIZE 0x11c /* GPIO_IFC - GPIO_PA_BASE */
+
+irqreturn_t gpio_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
-	printk("irq: %d", irq);
+	printk("irq: %d\n", irq);
+
+	/* Clear interrupt */
+	iowrite32(*GPIO_IF, GPIO_IFC);
 
 	return IRQ_HANDLED;
-}
-
-static int gamepad_probe(struct platform_device *dev) 
-{
-	int irq_gpio_even;
-	int irq_gpio_odd;
-	int irq_even_res;
-	int irq_odd_res;
-
-	irq_gpio_even = platform_get_irq(dev, 0);
-	irq_gpio_odd = platform_get_irq(dev, 1);
-
-	irq_even_res = register_irq(irq_gpio_even);
-
-	printk("even: %d\n", irq_even_res);
-
-	irq_odd_res = register_irq(irq_gpio_odd);
-
-	printk("odd: %d\n", irq_odd_res);
-
-	return 0;
-}
-
-static int gamepad_remove(struct platform_device *dev) 
-{
-	return 0;
-}
-
-static const struct of_device_id device_table[] = {
-	{ .compatible = "tdt4258", },
-	{},
-};
-
-MODULE_DEVICE_TABLE(of, device_table);
-
-static struct platform_driver gamepad_driver = {
-	.probe = gamepad_probe,
-	.remove = gamepad_remove,
-	.driver = {
-		.name = DEVICE_NAME,
-		.owner = THIS_MODULE,
-		.of_match_table = device_table,
-	},
-};
-
-int register_irq(int irq_num)
-{
-	int result = request_irq(irq_num, (irq_handler_t) gpio_handler, IRQF_TRIGGER_MASK, DEVICE_NAME, &gamepad_driver);
-	if (result < 0) {
-		printk(KERN_ALERT "%s: request_irq failed with %d\n", __func__, result);
-	}
-
-	return result;
 }
 
 /*
@@ -80,9 +33,45 @@ int register_irq(int irq_num)
 
 static int __init gamepad_module_init(void)
 {
-	/*printk("Hello World, here is your module speaking\n");*/
 
-	return platform_driver_register(&gamepad_driver);
+	struct resource *io_region;
+	int irq_request_res;
+
+	io_region = request_mem_region(GPIO_PA_BASE, IO_REGION_SIZE, DEVICE_NAME);
+
+	if (io_region == NULL)
+	{
+		printk("Unable to allocate IO memory\n");
+		return -1;
+	}
+
+	ioremap_nocache(io_region->start, IO_REGION_SIZE);
+
+	/* Interrupts */
+
+	irq_request_res = request_irq(17, (irq_handler_t) gpio_handler, 0, DEVICE_NAME, 0);
+	if (irq_request_res < 0) {
+		printk(KERN_ALERT "%s: request_irq failed with %d\n", __func__, irq_request_res);
+	}
+
+	irq_request_res = request_irq(18, (irq_handler_t) gpio_handler, 0, DEVICE_NAME, 0);
+	if (irq_request_res < 0) {
+		printk(KERN_ALERT "%s: request_irq failed with %d\n", __func__, irq_request_res);
+	}
+
+	/* Set pins 0-7 of port C to input */ 
+	iowrite32(0x33333333, GPIO_PC_MODEL);
+
+	/* Enable internal pull-up */
+	iowrite32(0xff, GPIO_PC_DOUT);
+
+	/* Enable GPIO interrupts on button down and up */
+	iowrite32(0x22222222, GPIO_EXTIPSELL);
+	iowrite32(0xff, GPIO_EXTIRISE);
+	iowrite32(0xff, GPIO_EXTIFALL);
+	iowrite32(0xff, GPIO_IEN);
+
+	return 0;
 }
 
 /*
@@ -94,7 +83,8 @@ static int __init gamepad_module_init(void)
 
 static void __exit gamepad_module_cleanup(void)
 {
-	 printk("Short life for a small module...\n");
+	release_mem_region(GPIO_PA_BASE, IO_REGION_SIZE);
+	printk("Short life for a small module...\n");
 }
 
 module_init(gamepad_module_init);
