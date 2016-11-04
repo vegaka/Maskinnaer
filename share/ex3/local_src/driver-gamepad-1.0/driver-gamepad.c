b4e3515e1347481798eb3196ce63819f9f216ac6
Vegard Seim Karstang <vegard.karstang@gmail.com>
2016-11-04 09:25:56 +0100

    Added code to make the driver visible to user-space. Added makefile rules for the game, and started work on signals.

--- share/ex3/local_src/driver-gamepad-1.0/driver-gamepad.c
+++ share/ex3/local_src/driver-gamepad-1.0/driver-gamepad.c
@@ -3,8 +3,12 @@
 #include <linux/init.h>
 #include <linux/ioport.h>
 #include <linux/interrupt.h>
-
+#include <linux/fs.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
+#include <linux/signal.h>
 #include <asm/io.h>
+#include <asm/siginfo.h>
 
 #include "efm32gg.h"
 
@@ -14,13 +18,52 @@
 
 irqreturn_t gpio_handler(int irq, void *dev_id, struct pt_regs *regs)
 {
+
 	printk("irq: %d\n", irq);
 
 	/* Clear interrupt */
 	iowrite32(*GPIO_IF, GPIO_IFC);
 
+	signal_game();
+
 	return IRQ_HANDLED;
 }
+
+void signal_game()
+{
+	int ret;
+	ret = send_sig_info(SIGUSR1, );
+}
+
+static int gamepad_open(struct inode *inode, struct file *file) {
+    printk(KERN_INFO "open sample char device\n");
+    return 0;
+}
+
+static int gamepad_release(struct inode *inode, struct file *file) {
+    printk(KERN_INFO "release sample char device\n");
+    return 0;
+}
+
+static ssize_t gamepad_read(struct file *file, char __user *data, size_t size, loff_t *offset) {
+    return size;
+}
+
+static ssize_t gamepad_write(struct file *file, const char __user *data, size_t size, loff_t *offset) {
+    return size;
+}
+
+static struct file_operations driver_fops = {
+	.owner = THIS_MODULE,
+	.read = gamepad_read,
+	.write = gamepad_write,
+	.open = gamepad_open,
+	.release = gamepad_release
+};
+
+static dev_t driver_major;
+static struct class *driver_class;
+static struct cdev *driver_cdev;
 
 /*
  * template_init - function to insert this module into kernel space
@@ -33,6 +76,8 @@ irqreturn_t gpio_handler(int irq, void *dev_id, struct pt_regs *regs)
 
 static int __init gamepad_module_init(void)
 {
+
+	int result; // Used to check result from functions
 
 	struct resource *io_region;
 	int irq_request_res;
@@ -71,6 +116,27 @@ static int __init gamepad_module_init(void)
 	iowrite32(0xff, GPIO_EXTIFALL);
 	iowrite32(0xff, GPIO_IEN);
 
+	result = alloc_chrdev_region(&driver_major, 0, 1, DEVICE_NAME);
+	if (result < 0) {
+		printk(KERN_ALERT "alloc_chrdev_region failed.\n");
+	}
+
+	driver_cdev = cdev_alloc();
+	cdev_init(driver_cdev, &driver_fops);	
+	result = cdev_add(driver_cdev, driver_major, 1);
+	if (result < 0) {
+		printk(KERN_ALERT "Failed to add cdev\n");
+	}
+
+	driver_class = class_create(THIS_MODULE, DEVICE_NAME);
+	if (!driver_class) {
+		printk(KERN_ALERT "Failed to create class\n");
+	}
+
+	if (!device_create(driver_class, NULL, driver_major, NULL, DEVICE_NAME)) {
+		printk(KERN_ALERT "Failed to create device\n");
+	}
+
 	return 0;
 }
 
@@ -83,8 +149,12 @@ static int __init gamepad_module_init(void)
 
 static void __exit gamepad_module_cleanup(void)
 {
+	device_destroy(driver_class, driver_major);
+	class_destroy(driver_class);
+	cdev_del(driver_cdev);
 	release_mem_region(GPIO_PA_BASE, IO_REGION_SIZE);
-	printk("Short life for a small module...\n");
+	unregister_chrdev_region(driver_major, 1);
+	printk(KERN_DEBUG "Exiting gamepad kernel module\n");
 }
 
 module_init(gamepad_module_init);
--- share/ex3/local_src/game-1.0/game.c
+++ share/ex3/local_src/game-1.0/game.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <signal.h>
+
+#define GAMEPAD_NAME "/dev/TDT4258"
+
+void driver_signal_handler(int signum)
+{
+	printf("Recieved signal: %d\n", signum);
+}
+
 
 int main(int argc, char *argv[])
 {
-	printf("Hello World, I'm game!\n");
+	/* printf("Hello World, I'm game!\n"); */
+
+	int gamepad_desc;
+
+	gamepad_desc = open(GAMEPAD_NAME, 0);
+
+	if (gamepad_desc < 0) {
+		printf("Can't open gamepad driver\n");
+		exit(-1);
+	}
+
+	signal(SIGUSR1, driver_signal_handler);
+	
+	while(1);
 
 	exit(EXIT_SUCCESS);
 }
