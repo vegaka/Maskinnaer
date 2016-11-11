08bfd6683c2f842fc8ca0a1be13879ea3ebebb03
Vegard Seim Karstang <vegard.karstang@gmail.com>
2016-11-11 10:01:40 +0100

    Added drawing rectangles, and started on the game.

--- share/ex3/local_src/game-1.0/game.c
+++ share/ex3/local_src/game-1.0/game.c
@@ -16,11 +16,31 @@
 #define HEIGHT 240
 #define BIT_DEPTH 16
 
+#define PADDLE_HEIGHT 30
+#define PADDLE_WIDTH 10
+
+#define BALL_SIZE 8
+
+enum Direction { STAY, UP, DOWN };
+
 int gamepad_desc;
 short buttons;
 
 int fb_desc;
-char *fb_mmap;
+unsigned short *fb_mmap;
+
+struct gameobject
+{
+	int x;
+	int y;
+	int w;
+	int h;
+	int dx;
+	int dy;
+	int r;
+	int g;
+	int b;
+};
 
 void driver_signal_handler(int signum)
 {
@@ -30,13 +50,94 @@ void driver_signal_handler(int signum)
 	printf("Read buttons: %d\n", buttons);
 }
 
+void draw_rect(int x, int y, int w, int h, int r, int g, int b)
+{
+	int i;
+	int j;
+	unsigned short color = (r << 11) | (g << 5) | b;
+
+	for (i = y; i < y + h; i++) {
+    		for (j = x; j < x + w; j++) {
+    			*(fb_mmap + (i * WIDTH + j)) = color;
+    		}
+    	}
+}
+
+void clear_object(struct gameobject *object)
+{
+	draw_rect(object->x, object->y, object->w, object->h, 0, 0, 0);
+}
+
+
+void draw_gameobject(struct gameobject *object)
+{
+	draw_rect(object->x, object->y, object->w, object->h, object->r, object->g, object->b);
+}
+
+void init_game(struct gameobject *player1, struct gameobject *player2, struct gameobject *ball)
+{
+	player1->x = 20;
+	player1->y = (HEIGHT / 2) - (PADDLE_HEIGHT / 2);
+	player1->w = PADDLE_WIDTH;
+	player1->h = PADDLE_HEIGHT;
+	player1->dx = 0;
+	player1->dy = 0;
+	player1->r = 0xff;
+	player1->g = 0;
+	player1->b = 0;
+
+	player2->x = WIDTH - 20 - PADDLE_WIDTH;
+	player2->y = (HEIGHT / 2) - (PADDLE_HEIGHT / 2);
+	player2->w = PADDLE_WIDTH;
+	player2->h = PADDLE_HEIGHT;
+	player2->dx = 0;
+	player2->dy = 0;
+	player2->r = 0;
+	player2->g = 0;
+	player2->b = 0xff;
+
+	ball->x = (WIDTH / 2) - (BALL_SIZE / 2);
+	ball->y = (HEIGHT / 2) - (BALL_SIZE / 2);
+	ball->w = BALL_SIZE;
+	ball->h = BALL_SIZE;
+	ball->dx = 0;
+	ball->dy = 0;
+	ball->r = 0xff;
+	ball->g = 0xff;
+	ball->b = 0xff;
+}
+
+/* Buttons: Last four bits indicate which buttons are pressed for the specific player */
+/* A zero means the button is pressed */
+void update_player(struct gameobject *player, int buttons)
+{
+	int up;
+	int down;
+
+	up = (~buttons) & (1 << 1);
+	down = (~buttons) & (1 << 3);
+
+	if (up && down) {
+		player->dy = 0;
+	} else if (up) {
+		player->dy -= 1;
+	} else if (down) {
+		player->dy += 1;
+	} else {
+		player->dy = 0;
+	}
+
+	// Check bounds
+
+	player->y += player->dy;
+
+}
 
 int main(int argc, char *argv[])
 {
 	/* printf("Hello World, I'm game!\n"); */
 
 	int file_flags;
-	int i, j, r, g, b;
 
 	gamepad_desc = open(GAMEPAD_NAME, 0);
 
@@ -57,31 +158,49 @@ int main(int argc, char *argv[])
 		exit(-1);
 	}
 
-	fb_mmap = (char *) mmap(NULL, WIDTH * HEIGHT * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fb_desc, 0);
+	fb_mmap = (unsigned short *) mmap(NULL, WIDTH * HEIGHT * 2, PROT_READ | PROT_WRITE, MAP_SHARED, fb_desc, 0);
 	if ((int) fb_mmap == -1) {
         	printf("Error: failed to map framebuffer device to memory\n");
         	printf("Error number: %d\n", errno);
         	exit(-1);
     	}
 
-    	for (i = 0; i < HEIGHT; i++) {
-    		for (j = 0; j < WIDTH; j++) {
-    			r = 0b11111;
-    			g = 0b111111;
-    			b = 0b11111;
-    			unsigned short color = (r << 11) | (g << 5) | b;
-    			*(fb_mmap + (i * WIDTH + j) * 2) = color;
-    		}
-    	}
+    	struct gameobject player1;
+    	struct gameobject player2;
+    	struct gameobject ball;
+
+    	init_game(&player1, &player2, &ball);
+
+    	ball.dx = 3;
 
     	struct fb_copyarea rect;
-    	rect.dx = 0;
-    	rect.dy = 0;
-    	rect.width = WIDTH;
-    	rect.height = HEIGHT;
-    	ioctl(fb_desc, 0x4680, &rect);
 
-	while(1);
+    	/* Game loop */
+	while(1) {
+		clear_object(&player1);
+		clear_object(&player2);
+		clear_object(&ball);
+
+		ball.x += ball.dx;
+		if (ball.x + ball.w > WIDTH) {
+			ball.x = 0;
+		}
+
+		update_player(&player1, buttons);
+		update_player(&player2, buttons >> 4);
+
+		draw_gameobject(&player1);
+    		draw_gameobject(&player2);
+    		draw_gameobject(&ball);
+
+    		rect.dx = 0;
+    		rect.dy = 0;
+    		rect.width = WIDTH;
+    		rect.height = HEIGHT;
+    		ioctl(fb_desc, 0x4680, &rect);
+
+    		//usleep(30 * 1000);
+	}
 
 	munmap(fb_mmap, WIDTH * HEIGHT * 2);
 	close(fb_desc);
